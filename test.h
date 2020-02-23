1f3c25ed01a9be06daf25b6de10533d515a57a6a
John Bland <jpbland@mtu.edu>
2020-02-23 00:33:27 +0000

    test pack and unpack methods

---
+++ test.h
@@ -0,0 +1,9 @@
+void test_pack();
+void test_pack_relay();
+void test_unpack();
+void test_unpack_relay();
+void compare_cells( unsigned char* packed_one, unsigned char* packed_two, int length );
+void fill_to_length( unsigned char* buffer, int length );
+void fill_to_length_short( unsigned short* buffer, int length );
+void compare_to_length( unsigned char* buffer, int length );
+void compare_to_length_short( unsigned short* buffer, int length );
